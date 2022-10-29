c0f38280f3b9485cc2fea097e7b461f6d5af5630
Rommelka <101477449+Rommelka@users.noreply.github.com>
2022-10-29 08:37:25 +0000

    Update Main.java

--- untitled/src/main/java/Main.java
+++ untitled/src/main/java/Main.java
@@ -4,7 +4,7 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 
 public class Main{
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
     System.out.println("лволдвталвььсьвсвы");
     }
 }
