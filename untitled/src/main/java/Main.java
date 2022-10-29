f6340a8f9110c3b532576aad9141d3b8213cb7be
Rommelka <101477449+Rommelka@users.noreply.github.com>
2022-10-29 09:17:21 +0000

    Update Main.java

--- untitled/src/main/java/Main.java
+++ untitled/src/main/java/Main.java
@@ -1,5 +1,5 @@
-public class Main{
-    public static void main(String[] args) throws InterruptedException {
-    System.out.println("kadsopokpas");
+public class wds {
+    public static void main(String[] args) {
+        System.out.println("kadsopokpas");
     }
 }
